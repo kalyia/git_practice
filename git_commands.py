b21626b5ac346dbf553c9b7dee5491659990891a
Kalyia <bekieva934@gmail.com>
2022-05-13 17:36:29 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+git init - .git
+git remote add name url
+git pull origin master
+git status
+git add
+git add file_name
+git add .
+git commit -m "comment"
+git branch
+git branch name_branch
+git checkout name_branch
+git push name_branch
+git push origin master
+git reset file_name
